60d001b858fcaa60b4932a7f4db8d9b7fd853963
ruanshiron <sandt26@gmail.com>
2022-01-07 09:39:45 +0700

    First commit

---
+++ spec/currency_numberal_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe CurrencyNumberal do
+  it "has a version number" do
+    expect(CurrencyNumberal::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end

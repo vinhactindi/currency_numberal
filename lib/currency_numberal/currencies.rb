563bcd502f02581fccce0c791617da0338de67fb
ruanshiron <sandt26@gmail.com>
2022-01-11 15:45:01 +0700

    disbale rubocop

--- lib/currency_numberal/currencies.rb
+++ lib/currency_numberal/currencies.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/ModuleLength
 module CurrencyNumberal
   CURRENCIES = {
     aed: {
@@ -632,3 +633,5 @@ module CurrencyNumberal
     }
   }.freeze
 end
+
+# rubocop:enable Metrics/ModuleLength
